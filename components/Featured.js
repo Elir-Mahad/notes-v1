39e874064a2c75b129c1a6d244df3bdf3b5ed2e9
elir-mahad <mmahad280@gmail.com>
2022-01-04 19:16:09 -0500

    Add featured component which is the placeholder for the landing page

---
+++ components/Featured.js
@@ -0,0 +1,10 @@
+import GitNotes from "../pages/git-notes";
+function Featured() {
+  return (
+    <div>
+      <GitNotes />
+    </div>
+  );
+}
+
+export default Featured;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Featured from "../components/Featured";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
           rel="stylesheet"
         />
       </Head>
+      <Featured />
     </div>
   );
 }
